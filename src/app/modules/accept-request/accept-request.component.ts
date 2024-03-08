57a4b353c2aafbb09b79252a87f0180942d588a4
karthikeyansuperman <karthikeyan.u.s4s@gmail.com>
2024-03-09 00:33:41 +0530

    o9.03.2024 mid knight commit

--- src/app/modules/accept-request/accept-request.component.ts
+++ src/app/modules/accept-request/accept-request.component.ts
@@ -64,12 +64,6 @@ export class AcceptRequestComponent implements OnInit {
       }
     });
 }
-
-GoNext: string = "button1";
-GoNextBtn(GoBtnName: string): void {
-  this.GoNext = GoBtnName;
-}
-  
   onSubmitStatus(form: NgForm) {
     if (form.valid && this.applyService.applyData) {
       this.applyService.PutStatus(form.value).subscribe();
