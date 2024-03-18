81c3c5e9f160b4bc2c7cafc4fded2eef37a0d231
Mogan <moganvig90@gmail.com>
2024-03-18 23:28:16 +0530

    commented the mdview in home component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     isProduction: true,
-    baseApiURL: 'https://localhost:7172/api/1.0/',
+    // baseApiURL: 'https://localhost:7172/api/1.0/',
+    baseApiURL: 'http://13.201.32.80:5001/api/1.0/',
     paymentURL : 'https://test.payu.in/merchant/postservice?form=2'
 };
